3800b93cca40546044f98b8c3de813fbc3fdf55f
Aria Radick <aradick@uoregon.edu>
2024-05-20 19:24:28 -0700

    added norm_energy, function overload

--- src/gaussians.jl
+++ src/gaussians.jl
@@ -1,4 +1,19 @@
 import SpecialFunctions: besselix
+import LinearAlgebra: dot
+
+struct GaussianF
+    c::Float64
+    uSph::Vector{Float64}
+    sigma::Float64
+end
+
+function (g::GaussianF)(uvec)
+    u_cart = sph_to_cart(uvec)
+    ui_cart = sph_to_cart(g.uSph)
+    duvec = u_cart .- ui_cart
+    du2 = dot(duvec, duvec)
+    return g.c/(sqrt(π)*g.sigma)^3 * exp(-du2/g.sigma^2)
+end
 
 function normG_nli_integrand(g::GaussianF, n, ell, u, basis::RadialBasis)
     u_i = g.uSph[1]
@@ -22,15 +37,9 @@ function normG_nli_integrand(g::GaussianF, n, ell, u, basis::RadialBasis)
     return measure * expfact * ivefact * radRn(n, ell, u, basis)
 end
 
-struct GaussianF
-    c::Float64
-    uSph::Vector{Float64}
-    sigma::Float64
-end
-
 function scale(g::GaussianF, scale_factor)
-    return GaussianF(g.c, [g.uSph[1]/scale_factor, g.uSph[2], g.uSph[3]], 
-            g.sigma/scale_factor)
+    return GaussianF(g.c, [g.uSph[1]*scale_factor, g.uSph[2], g.uSph[3]], 
+            g.sigma*scale_factor)
 end
 
 function getGnl(g::GaussianF, n, ell, radial_basis::RadialBasis;
@@ -38,7 +47,7 @@ function getGnl(g::GaussianF, n, ell, radial_basis::RadialBasis;
 
     umax = radial_basis.umax
     x_support = _base_of_support_n(n, radial_basis)
-    gx = scale(g, umax)
+    gx = scale(g, 1/umax)
 
     intg_Gnl(x) = normG_nli_integrand(gx, n, ell, x[1], radial_basis)
 
@@ -51,7 +60,7 @@ function getGnl(g::GaussianF, n, ell, radial_basis::RadialBasis;
     return gnl
 end
 
-function getGnlm(g::GaussianF, nlm, radial_basis::RadialBasis;
+function getFnlm(g::GaussianF, nlm, radial_basis::RadialBasis;
     integ_params::NamedTuple=(;))
 
     u_i, θ_i, φ_i = g.uSph
@@ -60,4 +69,37 @@ function getGnlm(g::GaussianF, nlm, radial_basis::RadialBasis;
     cY_i = g.c * ylm_real(ell, m, θ_i, φ_i)
 
     return cY_i * gnl / radial_basis.umax^3
+end
+
+function getFnlm(g::Vector{GaussianF}, nlm, radial_basis::RadialBasis;
+    integ_params::NamedTuple=(;))
+    return sum(getFnlm.(g, (nlm,), (radial_basis,); integ_params=integ_params))
+end
+
+norm_energy(g::GaussianF) = g.c/(g.sigma * sqrt(2*π))^3
+
+function norm_energy(g::Vector{GaussianF})
+    res = 0.0
+
+    for gi in g
+        s2i = gi.sigma^2
+        ui = sph_to_cart(gi.uSph)
+        u2i = dot(ui, ui)
+
+        for gj in g
+            s2j = gj.sigma^2
+            s2_ij = s2i * s2j / (s2i + s2j)
+
+            uj = sph_to_cart(gj.uSph)
+            u2j = dot(uj, uj)
+
+            u_ij = (s2j*ui + s2i*uj) / (s2i + s2j)
+            u2_ij = dot(u_ij, u_ij)
+
+            exp_ij = exp(u2_ij/s2_ij - u2i/s2i - u2j/s2j)
+
+            res += gi.c*gj.c*exp_ij / (π * (s2i+s2j))^(3/2)
+        end
+    end
+    return res
 end
