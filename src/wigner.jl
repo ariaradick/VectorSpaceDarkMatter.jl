f6e07c7eaad6134babd1b6d7b5a851539d5d7182
Aria Radick <aradick@uoregon.edu>
2024-07-16 15:14:37 -0700

    big G matrix speedup

--- src/wigner.jl
+++ src/wigner.jl
@@ -1,71 +1,70 @@
 
-function _Glmpm_from_Dl(Dl, ell, mp, m)
-    if mp < 0 && m < 0
-        return real(Dl[ell-mp+1, ell-m+1] - (-1)^mp * Dl[ell+mp+1, ell-m+1])
-    elseif mp < 0 && m == 0
-        return -sqrt(2)*imag(Dl[ell-mp+1, ell+1])
-    elseif mp < 0 && m > 0
-        return -imag(Dl[ell-mp+1, ell+m+1] - (-1)^mp * Dl[ell+mp+1, ell+m+1])
-    elseif mp == 0 && m < 0
-        return sqrt(2)*imag(Dl[ell+1, ell-m+1])
-    elseif mp==0 && m==0
-        return real(Dl[ell+1, ell+1])
-    elseif mp==0 && m>0
-        return sqrt(2)*real(Dl[ell+1, ell+m+1])
-    elseif mp>0 && m<0
-        return imag(Dl[ell+mp+1, ell-m+1] + (-1)^mp * Dl[ell-mp+1, ell-m+1])
-    elseif mp>0 && m==0
-        return sqrt(2)*real(Dl[ell+mp+1, ell+1])
-    elseif mp>0 && m>0
-        return real(Dl[ell+mp+1, ell+m+1] + (-1)^mp * Dl[ell-mp+1, ell+m+1])
+function _Gl_from_Dl!(Gl,Dl,ell)
+    for mp in -ell:-1
+        for m in -ell:-1
+            Gl[ell+mp+1, ell+m+1] = real(Dl[ell-mp+1, ell-m+1] - 
+                                    (-1)^mp * Dl[ell+mp+1, ell-m+1])
+        end
+        for m in 1:ell
+            Gl[ell+mp+1, ell+m+1] = -imag(Dl[ell-mp+1, ell+m+1] - 
+                                          (-1)^mp * Dl[ell+mp+1, ell+m+1])
+        end
+        Gl[ell+mp+1, ell+1] = -sqrt(2)*imag(Dl[ell-mp+1, ell+1])
     end
+    for mp in 1:ell
+        for m in -ell:-1
+            Gl[ell+mp+1, ell+m+1] = imag(Dl[ell+mp+1, ell-m+1] + 
+                                         (-1)^mp * Dl[ell-mp+1, ell-m+1])
+        end
+        for m in 1:ell
+            Gl[ell+mp+1, ell+m+1] = real(Dl[ell+mp+1, ell+m+1] + 
+                                         (-1)^mp * Dl[ell-mp+1, ell+m+1])
+        end
+        Gl[ell+mp+1, ell+1] = sqrt(2)*real(Dl[ell+mp+1, ell+1])
+    end
+    for m in -ell:-1
+        Gl[ell+1, ell+m+1] = sqrt(2)*imag(Dl[ell+1, ell-m+1])
+    end
+    for m in 1:ell
+        Gl[ell+1, ell+m+1] = sqrt(2)*real(Dl[ell+1, ell+m+1])
+    end
+    Gl[ell+1,ell+1] = real(Dl[ell+1, ell+1])
 end
 
+"Can be called with a pre-computed D matrix instead (see SphericalFunctions.jl)"
 function G_matrices(D_matrices, ell_max)
     G = zeros(Float64, size(D_matrices))
-    i = 1
-    for (ell, Dl) in zip(0:ell_max, D_iterator(D_matrices, ell_max))
-        for mp in -ell:ell
-            for m in -ell:ell
-                G[i] = _Glmpm_from_Dl(Dl, ell, mp, m)
-                i += 1
-            end
-        end
+    for (ell, Dl, Gl) in zip(0:ell_max, D_iterator(D_matrices, ell_max),
+                             D_iterator(G, ell_max))
+        _Gl_from_Dl!(Gl',Dl,ell)
     end
     return G
 end
 
+"""
+    G_matrices(R::T, ell_max) where T<:Union{Quaternion,Rotor}
+
+Calculates the analog of the Wigner D matrices for real spherical harmonics,
+which we refer to as the "G matrices" for a given rotation represented as a 
+quaternion (or rotor). If you'd like to use other rotations, see the relevant
+conversion functions in Quaternionic.jl
+"""
 function G_matrices(R::T, ell_max) where T<:Union{Quaternion,Rotor}
     D = D_matrices(R, ell_max)
     return G_matrices(D, ell_max)
 end
 
-function G_matrices(α::Real, β::Real, γ::Real, ell_max)
-    Rq = from_euler_angles(α,β,γ)
-    G_matrices(Rq, ell_max)
-end
-
 function G_matrices!(G, D::Vector, ell_max)
-    i = 1
-    for (ell, Dl) in zip(0:ell_max, D_iterator(D, ell_max))
-        for mp in -ell:ell
-            for m in -ell:ell
-                G[i] = _Glmpm_from_Dl(Dl, ell, mp, m)
-                i += 1
-            end
-        end
+    for (ell, Dl, Gl) in zip(0:ell_max, D_iterator(D, ell_max),
+                             D_iterator(G, ell_max))
+        _Gl_from_Dl!(Gl',Dl,ell)
     end
 end
 
 function G_matrices!(G, D::Tuple)
     ell_max = D[2]
-    i = 1
-    for (ell, Dl) in zip(0:ell_max, D_iterator(D[1], ell_max))
-        for mp in -ell:ell
-            for m in -ell:ell
-                G[i] = _Glmpm_from_Dl(Dl, ell, mp, m)
-                i += 1
-            end
-        end
+    for (ell, Dl, Gl) in zip(0:ell_max, D_iterator(D[1], ell_max),
+                             D_iterator(G, ell_max))
+        _Gl_from_Dl!(Gl',Dl,ell)
     end
 end
