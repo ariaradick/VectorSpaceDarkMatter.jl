3b0ccba88c5997c0ee62cca39baf5dbd976db432
Aria Radick <aradick@uoregon.edu>
2024-12-22 11:50:05 -0800

    fixed typo in ProjectedF docstring

--- src/projection.jl
+++ src/projection.jl
@@ -1,5 +1,5 @@
 """
-    ProjectedF{A, B}(fnlm::Matrix{A}, radial_basis::B)
+    ProjectedF{A, B}(fnlm::Matrix{A}, lm, radial_basis::B)
 
 Stores the ``\\langle f | n \\ell m \\rangle`` coefficients and the radial basis
 that was used to calculate them. It is assumed that spherical harmonics were 
