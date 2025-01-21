7509935a04aa4ea59cec6eb89749696d9f7f085e
Aria Radick <aradick@uoregon.edu>
2025-01-20 17:40:13 -0800

    added support for velocity-dependent dark matter form factors

--- src/analytic_mcalI.jl
+++ src/analytic_mcalI.jl
@@ -1,15 +1,16 @@
-function _b_nk_int(n::Int, k::Int, x)
+function _b_nk_int(n, k, x)
     res = 0.0
     for j in 0:k
         comb = binomial(Float64(k),j)
-        ipower = j - n - k/2 + 1
+        ipower = j + (n - k)/2 + 1
         if ipower ≈ 0
             res += comb*log(x)
         else
             res += comb * x^ipower / ipower
         end
     end
-    return 0.5 * res
+    res *= 0.5
+    return res
 end
 
 function _c_alpha_int(α, x)
@@ -42,171 +43,29 @@ function _c_alpha_int(α, x)
     end
 end
 
-function _v_n_int(n, x)
-    if n==0
-        return 0.5*log(x) + x + 0.25*x^2
-    elseif n==1
-        return -0.5/x + log(x) + 0.5*x
-    elseif n==2
-        return -0.25/x^2 - 1/x + 0.5*log(x)
-    else
-        return 0.5*(x^(-n)/(-n)) + x^(-n+1)/(-n+1) + 0.5*x^(-n+2)/(-n+2)
-    end
-end
-
-function _s_n_int(n, x)
-    logfactor = 0.5*log(x/(1+2*x+x^2))
-    return (logfactor*_v_n_int(n, x) + 0.5*_c_alpha_int(-n, x)
-            + _c_alpha_int(1-n, x) + 0.5*_c_alpha_int(2-n, x))
-end
-
-"""
-    t_ln_vq_int(l::Integer, n, v12_star, q12_star)
-
-Rectangular integral T_{l,n}.
-
-With [v1,v2] in units of v_star = q_star/mX, [q1,q2] in units of
-    q_star = sqrt(2*mX*omegaS).
-
-Always v1 >= 1. Also require q1 > 0.
-"""
-function t_ln_vq_int(l::Integer, n, v12_star, q12_star)
-    v1,v2 = v12_star
-    q1,q2 = q12_star
-    x1 = q1^2
-    x2 = q2^2
+function _v_nm_int(n, m, x)
     res = 0.0
-    for k in (l%2):2:l
-        term_k = 2^(l-k) * gamma(0.5*(k+1+l)) / gamma(0.5*(k+1-l))
-        if k==2
-            termV = log(v2/v1) / (2 * gamma(l-1))
-            termQ = (_b_nk_int(n, 2, x2) - _b_nk_int(n, 2, x1))
-            res += termV*term_k*termQ
+    for j in 0:(m+2)
+        comb = binomial(m+2,j)
+        ipower = j + (n-m)/2
+        if ipower ≈ 0
+            res += comb*log(x)
         else
-            termV = ((v2^(2-k) - v1^(2-k))
-                     / ((2-k)*gamma(k+1)*gamma(l-k+1)))
-            termQ = (_b_nk_int(n, k, x2) - _b_nk_int(n, k, x1))
-            res += termV*term_k*termQ
+            res += comb * x^ipower / ipower
         end
     end
+    res *= 0.5
     return res
 end
 
-"""
-    u_ln_vq_int(l::Integer, n, v2_star, q12_star)
-
-Non-rectangular integral U_{l,n}, with lower bound v1 = v_min(q).
-
-With v2 in units of v_star = q_star/mX, [q1,q2] in units of
-    q_star = sqrt(2*mX*omegaS).
-"""
-function u_ln_vq_int(l::Integer, n, v2_star, q12_star)
-    v2 = v2_star # only need v2, v1 is irrelevant
-    q1,q2 = q12_star
-    x1 = q1^2
-    x2 = q2^2
-    sum = 0.0
-    for k in (l%2):2:l
-        term_k = (gamma(0.5*(k+1+l)) / gamma(0.5*(k+1-l))
-                  * 2.0^(l-k)/(gamma(k+1) * gamma(l-k+1)))
-        if k==2
-            summand = (log(2.0*v2)*_b_nk_int(n, 2, x2) + _s_n_int(n, x2)
-                       - log(2.0*v2)*_b_nk_int(n, 2, x1) - _s_n_int(n, x1))
-            sum += term_k*summand
-        else
-            summand = (v2^(2-k)*(_b_nk_int(n, k, x2) - _b_nk_int(n, k, x1))
-                       - 2.0^(k-2)*(_b_nk_int(n, 2, x2) - _b_nk_int(n, 2, x1)))
-            sum += term_k*summand/(2-k)
-        end
+function _s_nm_int(n, m, x)
+    res = _v_nm_int(n,m,x) * log(sqrt(x)/(1+x))
+    for j in 0:(m+2)
+        ipower = j + (n-m)/2
+        comb = binomial((m+2),j)
+        res += 0.5*comb*_c_alpha_int(ipower, x)
     end
-    return sum
-end
-
-"""
-    mI_star(ell, n, v12_star, q12_star)
-
-Dimensionless integral related to MathcalI.
-
-This is 'I^{(\\ell)}_\\star' without the prefactor (qBohr/qStar)**(2n)
-
-With v12, q12 in units of vStar, qStar.
-
-There are 0, 1, 2 or 3 regions that contribute to mcalI:
-    qA < (R1) < qB < (R2) < qC < (R3) < qD.
-    R2 is rectangular, bounded by v1 < v < v2. -> t_ln_vq_int
-    R1 and R3 are not rectangular: vMin(q) < v < v2. -> u_ln_vq_int
-If vmin(q) > v1 for all q1 < q < q2, then mcalI is given by u_ln_vq_int
-"""
-function mI_star(ell, n, v12_star, q12_star)
-    v1,v2 = v12_star
-    q1,q2 = q12_star
-    if v1==v2 || q1==q2
-        return 0.0 # No integration volume
-    end
-    if q2 < q1
-        error("q12 must be ordered.")
-    end
-    if v2 < v1
-        error("v12 must be ordered.")
-    end
-    include_R2 = true
-    if v2 < 1.0
-        # v2 is below the velocity threshold. mcaI=0
-        return 0.0
-    end
-    tilq_m,tilq_p = v2 - sqrt(v2^2-1.0), v2 + sqrt(v2^2-1.0)
-    if tilq_m > q2 || tilq_p < q1
-        # in this case v2 < vmin(q) for all q in [q1,q2]
-        return 0.0
-    end
-    # Else: there are some q satisfying vmin(q) < v2 in this interval.
-    if v1 < 1.0
-        # There is no v1 = vmin(q) solution for any real q
-        include_R2 = false
-    # Else: There are two real solutions to v1 = vmin(q)
-    else
-        q_m,q_p = v1 - sqrt(v1^2-1.0), v1 + sqrt(v1^2-1.0)
-        if q_m > q2 || q_p < q1
-            # in this case v1 < vmin(q) for all q in [q1,q2]
-            include_R2 = false
-        end
-    end
-    if !include_R2
-        q_A = max(q1, tilq_m)
-        q_B = min(q2, tilq_p)
-        return u_ln_vq_int(ell, n, v2, [q_A, q_B])
-    end
-    # Else: at least part of the integration volume is set by v1 < v.
-    q_a = max(q1, tilq_m)
-    q_b = max(q1, q_m) # q_m > tilq_m iff v2 > v1
-    q_c = min(q2, q_p) # q_p < tilq_p iff v2 > v1
-    q_d = min(q2, tilq_p)
-
-    includeRegion = [true, true, true]
-    if q_a==q_b
-        includeRegion[1] = false
-    end
-
-    if q_c==q_d
-        includeRegion[3] = false
-    end
-
-    if v1>1.0
-        if q_b == q_c
-            error("If q_b==q_c then there should be no R2 region...")
-        end
-    end
-    mI_0,mI_1,mI_2 = 0.0, 0.0, 0.0
-    if includeRegion[1]
-        mI_0 = u_ln_vq_int(ell, n, v2, [q_a, q_b])
-    end
-    if includeRegion[2]
-        mI_1 = t_ln_vq_int(ell, n, [v1,v2], [q_b, q_c])
-    end
-    if includeRegion[3]
-        mI_2 = u_ln_vq_int(ell, n, v2, [q_c, q_d])
-    end
-    return mI_0 + mI_1 + mI_2
+    return res
 end
 
 function T_matrix(ℓmax)
@@ -220,37 +79,34 @@ function T_matrix(ℓmax)
     return LowerTriangular(transpose(res))
 end
 
-function add_T_vector!(I, a, v1, v2, q1, q2; fdmn=0)
+function add_T_vector!(I, a, n, m, v1, v2, q1, q2)
     for i in eachindex(I)
-        if i==3
-            I[i] += a*(_b_nk_int(fdmn, 2, q2^2)-
-                     _b_nk_int(fdmn, 2, q1^2))*log(v2/v1)
+        if i==(m+3)
+            I[i] += a * (_b_nk_int(n, m+2, q2^2) - _b_nk_int(n, m+2, q1^2)) *
+                    log(v2/v1)
         else
             k = i-1
-            I[i] += a*(_b_nk_int(fdmn, k, q2^2)-
-                    _b_nk_int(fdmn, k, q1^2)) * 
-                    (v2^(2-k) - v1^(2-k)) / (2-k)
+            I[i] += a * (_b_nk_int(n, k, q2^2) - _b_nk_int(n, k, q1^2)) * 
+                    (v2^(m+2-k) - v1^(m+2-k)) / (m+2-k)
         end
     end
 end
 
-function add_U_vector!(I, a, v2, q1, q2; fdmn=0)
-    dB2 = _b_nk_int(fdmn,2,q2^2) - _b_nk_int(fdmn,2,q1^2)
+function add_U_vector!(I, a, n, m, v2, q1, q2)
+    dBm2 = _b_nk_int(n,m+2,q2^2) - _b_nk_int(n,m+2,q1^2)
     for i in eachindex(I)
-        if i==3
-            I[i] += a*(log(2*v2)*dB2 + _s_n_int(fdmn,q2^2) - 
-                    _s_n_int(fdmn,q1^2))
+        if i==(m+3)
+            I[i] += a*(log(2*v2)*dBm2 + _s_nm_int(n,m,q2^2) - 
+                    _s_nm_int(n,m,q1^2))
         else
             k = i-1
-            I[i] += a*(v2^(2-k) / (2-k) * (_b_nk_int(fdmn,k,q2^2) - 
-                    _b_nk_int(fdmn,k,q1^2)) - 2.0^(k-2)*dB2/ (2-k))
+            I[i] += a*(v2^(m+2-k)  * (_b_nk_int(n,k,q2^2) - 
+                    _b_nk_int(n,k,q1^2)) - 2.0^(k-m-2)*dBm2) / (m+2-k)
         end
     end
 end
 
-function not_mI_star!(I, a, n, v12_star, q12_star)
-    v1,v2 = v12_star
-    q1,q2 = q12_star
+function not_mI_star!(I, a, n, m, v1, v2, q1, q2)
     if v1==v2 || q1==q2
         return # No integration volume
     end
@@ -285,7 +141,7 @@ function not_mI_star!(I, a, n, v12_star, q12_star)
     if !include_R2
         q_A = max(q1, tilq_m)
         q_B = min(q2, tilq_p)
-        add_U_vector!(I, a, v2, q_A, q_B; fdmn=n)
+        add_U_vector!(I, a, n, m, v2, q_A, q_B)
     else
         # Else: at least part of the integration volume is set by v1 < v.
         q_a = max(q1, tilq_m)
@@ -299,12 +155,12 @@ function not_mI_star!(I, a, n, v12_star, q12_star)
             end
         end
 
-        add_T_vector!(I, a, v1, v2, q_b, q_c; fdmn=n)
+        add_T_vector!(I, a, n, m, v1, v2, q_b, q_c)
         if q_a ≠ q_b
-            add_U_vector!(I, a, v2, q_a, q_b; fdmn=n)
+            add_U_vector!(I, a, n, m, v2, q_a, q_b)
         end
         if q_c ≠ q_d
-            add_U_vector!(I, a, v2, q_c, q_d; fdmn=n)
+            add_U_vector!(I, a, n, m, v2, q_c, q_d)
         end
     end
 end
--- src/matrixcalc.jl
+++ src/matrixcalc.jl
@@ -1,9 +1,13 @@
 """
-    ModelDMSM(fdm_n, mX, mSM, deltaE)
+    ModelDMSM(beta, gamma, mX, mSM, deltaE)
 
 Stores the relevant model parameters.
 
-`fdm_n` : The power ``n`` of the dark matter form factor ``(\\alpha m_e / q)^{n}``
+`beta` : Two times the power of ``q`` in the dark matter form factor, 
+         ``F_{DM} \\sim (\\alpha m_e / q)^{\\beta/2}``
+
+`gamma` : Two times the power of ``v`` in the dark matter form factor, 
+         ``F_{DM} \\sim v^{\\gamma/2}``
 
 `mX` : dark matter mass in eV
 
@@ -12,10 +16,15 @@ Stores the relevant model parameters.
 `deltaE` : discrete final state energy in eV
 """
 struct ModelDMSM
-    fdm_n::Int
+    beta::Int
+    gamma::Int
     mX::Float64
     mSM::Float64
     deltaE::Float64
+end
+
+function ModelDMSM(fdm_n, mX, mSM, deltaE)
+    ModelDMSM(-2*fdm_n, 0, mX, mSM, deltaE)
 end
 
 function _getABval(n, basis::Wavelet)
@@ -27,64 +36,12 @@ function _getABval(n, basis::Tophat)
     return tophat_value(x_n, x_np1)
 end
 
-function getI_lvq_analytic(lnvnq, model::ModelDMSM, v_basis::RadialBasis, 
-        q_basis::RadialBasis)
-    mX = model.mX
-    fdm_n = model.fdm_n
-    DeltaE = model.deltaE
-    mSM = model.mSM
-
-    ell, nv, nq = lnvnq
-    v0 = v_basis.umax
-    q0 = q_basis.umax
-
-    muSM = mX*mSM/(mX+mSM)
-
-    qStar = sqrt(2*mX*DeltaE)
-    vStar = qStar/mX
-
-    commonFactor = ((q0/v0)^3/(2*mX*muSM^2) * (2*DeltaE/(q0*v0))^2
-                        *(q0_fdm/qStar)^(2*fdm_n))
-    
-    v_base = _base_of_support_n(nv, v_basis).*v0
-    AB_v = _getABval(nv, v_basis)
-    q_base = _base_of_support_n(nq, q_basis).*q0
-    AB_q = _getABval(nq, q_basis)
-
-    res = 0.0
-    for i in eachindex(AB_v)
-        for j in eachindex(AB_q)
-            v12_star = [v_base[i], v_base[i+1]] ./ vStar
-            q12_star = [q_base[j], q_base[j+1]] ./ qStar
-            res += AB_v[i]*AB_q[j]*mI_star(ell, fdm_n, v12_star, q12_star)
-        end
-    end
-    return commonFactor*res
-end
-
-function I_lvq(lnvnq_max, model::ModelDMSM, v_basis::RadialBasis, 
-        q_basis::RadialBasis)
-    l_max, nv_max, nq_max = lnvnq_max
-
-    res = zeros(Float64, (nv_max+1, nq_max+1, l_max+1))
-
-    Threads.@threads for ell in 0:l_max
-        Threads.@threads for nq in 0:nq_max
-            Threads.@threads for nv in 0:nv_max
-                res[nv+1, nq+1, ell+1] = getI_lvq_analytic((ell, nv, nq),
-                                         model, v_basis, q_basis)
-            end
-        end
-    end
-
-    return res
-end
-
 function _fill_I!(Imat, tmat, lnvnq_max, model::ModelDMSM, 
                   v_basis::RadialBasis, q_basis::RadialBasis)
     nv_max, nq_max = lnvnq_max[2:3]
     mX = model.mX
-    fdm_n = model.fdm_n
+    n = model.beta
+    m = model.gamma
     DeltaE = model.deltaE
 
     v0 = v_basis.umax
@@ -93,20 +50,23 @@ function _fill_I!(Imat, tmat, lnvnq_max, model::ModelDMSM,
     qStar = sqrt(2*mX*DeltaE)
     vStar = qStar/mX
 
-    @Threads.threads for nq in 0:nq_max
+    for nq in 0:nq_max
         q_base = _base_of_support_n(nq, q_basis).*q0
         AB_q = _getABval(nq, q_basis)
 
-        @Threads.threads for nv in 0:nv_max
+        for nv in 0:nv_max
             v_base = _base_of_support_n(nv, v_basis).*v0
             AB_v = _getABval(nv, v_basis)
 
             temp = @view Imat[:, nv+1, nq+1]
             for i in eachindex(AB_v)
                 for j in eachindex(AB_q)
-                    v12_star = [v_base[i], v_base[i+1]] ./ vStar
-                    q12_star = [q_base[j], q_base[j+1]] ./ qStar
-                    not_mI_star!(temp, AB_v[i]*AB_q[j], fdm_n, v12_star, q12_star)
+                    v1_star = v_base[i] / vStar
+                    v2_star = v_base[i+1] / vStar
+                    q1_star = q_base[j] / qStar
+                    q2_star = q_base[j+1] / qStar
+                    not_mI_star!(temp, AB_v[i]*AB_q[j], n, m, v1_star, v2_star,
+                                 q1_star, q2_star)
                 end
             end
 
@@ -120,35 +80,8 @@ function kinematic_I(lnvnq_max, model::ModelDMSM,
     l_max, nv_max, nq_max = lnvnq_max
 
     mX = model.mX
-    fdm_n = model.fdm_n
-    DeltaE = model.deltaE
-    mSM = model.mSM
-
-    v0 = v_basis.umax
-    q0 = q_basis.umax
-
-    muSM = mX*mSM/(mX+mSM)
-
-    qStar = sqrt(2*mX*DeltaE)
-
-    commonFactor = ((q0/v0)^3/(2*mX*muSM^2) * (2*DeltaE/(q0*v0))^2
-            *(q0_fdm/qStar)^(2*fdm_n))
-
-    tmatrix = T_matrix(l_max)
-    res = zeros(Float64, (l_max+1, nv_max+1, nq_max+1))
-    _fill_I!(res, tmatrix, lnvnq_max, model, v_basis::RadialBasis, 
-             q_basis::RadialBasis)
-    @. res *= commonFactor
-
-    return permutedims(res,[2,3,1])
-end
-
-function _oldI(lnvnq_max, model::ModelDMSM, 
-    v_basis::RadialBasis, q_basis::RadialBasis)
-    l_max, nv_max, nq_max = lnvnq_max
-
-    mX = model.mX
-    fdm_n = model.fdm_n
+    n = model.beta
+    m = model.gamma
     DeltaE = model.deltaE
     mSM = model.mSM
 
@@ -160,33 +93,14 @@ function _oldI(lnvnq_max, model::ModelDMSM,
     qStar = sqrt(2*mX*DeltaE)
     vStar = qStar/mX
 
-    commonFactor = ((q0/v0)^3/(2*mX*muSM^2) * (2*DeltaE/(q0*v0))^2
-            *(q0_fdm/qStar)^(2*fdm_n))
+    commonFactor = ((q0/v0)^3/(2*mX*muSM^2) * (2*DeltaE/(q0*v0))^2 *
+                    (qStar/q0_fdm)^n * vStar^m)
 
     tmatrix = T_matrix(l_max)
+    res = zeros(Float64, (l_max+1, nv_max+1, nq_max+1))
+    _fill_I!(res, tmatrix, lnvnq_max, model, v_basis::RadialBasis, 
+             q_basis::RadialBasis)
+    @. res *= commonFactor
 
-    res = zeros(Float64, (nv_max+1, nq_max+1, l_max+1))
-
-    for nq in 0:nq_max
-        q_base = _base_of_support_n(nq, q_basis).*q0
-        AB_q = _getABval(nq, q_basis)
-
-        for nv in 0:nv_max
-            v_base = _base_of_support_n(nv, v_basis).*v0
-            AB_v = _getABval(nv, v_basis)
-
-            temp = zeros(Float64, l_max+1)
-            for i in eachindex(AB_v)
-                for j in eachindex(AB_q)
-                    v12_star = [v_base[i], v_base[i+1]] ./ vStar
-                    q12_star = [q_base[j], q_base[j+1]] ./ qStar
-                    # temp .+= AB_v[i]*AB_q[j].*not_mI_star(l_max, fdm_n, v12_star, q12_star)
-                    not_mI_star!(temp, AB_v[i]*AB_q[j], fdm_n, v12_star, q12_star)
-                end
-            end
-
-            res[nv+1, nq+1, :] = (tmatrix * temp)
-        end
-    end
-    return commonFactor .* res
+    return permutedims(res,[2,3,1])
 end
