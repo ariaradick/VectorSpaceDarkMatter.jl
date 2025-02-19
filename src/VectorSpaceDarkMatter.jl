22a7201f33038a8408a3eefc4cf9cf59355d9cf5
Aria Radick <aradick@uoregon.edu>
2025-02-18 16:04:46 -0800

    remove PartialRate export

--- src/VectorSpaceDarkMatter.jl
+++ src/VectorSpaceDarkMatter.jl
@@ -13,7 +13,7 @@ using SphericalFunctions
 
 export Wavelet, Tophat, f_uSph, GaussianF, f2_norm, ProjectF,
        ProjectedF, FCoeffs, update!, writeFnlm, readFnlm, rate,
-       ModelDMSM, McalK, partial_rate, readK, writeK, PartialRate
+       ModelDMSM, McalK, partial_rate, readK, writeK
 
 include("units.jl")
 include("utils.jl")
